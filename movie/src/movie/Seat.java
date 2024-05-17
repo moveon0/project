9d949029dc2624cb93910ef869dbb5b6509b158e
moveon0 <yunyj1224@gmail.com>
2024-05-17 16:42:26 +0900

    img추가

--- movie/src/movie/Seat.java
+++ movie/src/movie/Seat.java
@@ -1,20 +1,27 @@
 package movie;
 
+
+
+
 import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.util.ArrayList;
+import java.util.List;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
+import javax.swing.*;
 import javax.swing.border.LineBorder;
 
 class PersonPanel extends JPanel {
     private JLabel personCountLabel;
     private static int totalPersonCount = 0;
+    static int checked = 0;
+    static String tickets = "";
 
     public PersonPanel() {
         setLayout(new FlowLayout());
@@ -72,7 +79,7 @@ class Adult_Panel extends JPanel {
     public Adult_Panel() {
         setLayout(null);
 
-        setBounds(40, 625, 300, 50);
+        setBounds(40, 625, 200, 50);
 
         JLabel Adult = new JLabel("인원");
         Adult.setBounds(10, 0, 50, 50);
@@ -93,7 +100,7 @@ class total_panel extends JPanel {
     static JLabel total;
 
     public total_panel() {
-        setBounds(450, 600, 300, 100);
+        setBounds(250, 600, 300, 80);
         setLayout(null);
         setBackground(Color.white);
         JLabel amount = new JLabel();
@@ -121,49 +128,96 @@ class Seat_Button extends JButton{
 		setText(seat);
 	}
 }
-
 class Seat_Panel extends JPanel{
 	public Seat_Panel() {
+		PersonPanel.checked = 0;
+		PersonPanel.tickets = "";
 		setLayout(null);
-		setBounds(325, 100, 500, 300);
+		setBounds(130, 30, 950, 400);
 		setBackground(Color.white);
 		
 		JLabel title = new JLabel("좌석 선택");
 		title.setBounds(160, 20, 200, 60);
 		Font font = new Font("Malgun Gothic",Font.BOLD,30);
 		title.setFont(font);
+
+		int abccnt = 0;
+		int cnt = 0;
+		int seatNum = 0;
+		int seatcnt = Integer.parseInt(String.valueOf(150));
+		final JCheckBox[] seats = new JCheckBox[seatcnt];
+		int x = (int) Math.ceil(seatcnt / 20.0);
+		int y = (int) Math.ceil(seatcnt / Math.ceil(seatcnt / 20.0));
+		JPanel panel = new JPanel(new GridLayout(x, y));
+		String[] abc = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P" };
+		for (int i = 0; i < seats.length; i++) {
+			if (cnt % y == 0 && i != 0) {
+				abccnt++;
+				seatNum = 1;
+				seats[i] = new JCheckBox(abc[abccnt] + String.valueOf(seatNum));
+				panel.add(seats[i]);
+			} else {
+				String check = abc[abccnt];
+				seats[i] = new JCheckBox(abc[abccnt] + String.valueOf(seatNum));
+				if (check.equals("A")) {
+					seats[i] = new JCheckBox(abc[abccnt] + String.valueOf(seatNum + 1));
+					panel.add(seats[i]);
+				}
+				panel.add(seats[i]);
+				seats[i].addItemListener(new ItemListener() {
+					@Override
+					public void itemStateChanged(ItemEvent e) {
+						int end = 0;
+						end = e.paramString().indexOf("],stateChange");
+						String a = e.paramString().charAt(end-3) +""+ e.paramString().charAt(end-2) + e.paramString().charAt(end-1)+"/";
+						a = a.replaceAll("=", "");
+						
+						if(e.getStateChange() == 1) {
+							PersonPanel.checked++;
+							if(PersonPanel.checked > 5) {
+				  				JOptionPane.showMessageDialog(null, "티켓은 5명까지만 선택 가능합니다.", "경고 메시지", JOptionPane.WARNING_MESSAGE);
+							}
+							PersonPanel.tickets = PersonPanel.tickets += a;
+							System.out.println(PersonPanel.tickets);
+						}else {
+							PersonPanel.checked--;
+							PersonPanel.tickets = PersonPanel.tickets.replaceAll(a, "");
+							System.out.println(PersonPanel.tickets);
+						}
+						
+						Your_Choice1.updateSeat1(PersonPanel.tickets);
+
+					}
+				});
+			}
+			cnt++;
+			seatNum++;
+			seats[i].setBorderPainted(false);
+		}
+		add(panel);
+//        getContentPane().setLayout(null);
+//        setLocation(null);
+//		repaint();
 		
-		Seat_Button seat1 = new Seat_Button("A1");
-		seat1.setBounds(80, 150, 100, 50);
-		Seat_Button seat2 = new Seat_Button("A2");
-		seat2.setBounds(280, 150, 100, 50);
-		
-		seat1.addActionListener(e -> {
-
-	         Your_Choice1.updateSeat1(seat1.getText());
-
-	      });
-		
-		seat2.addActionListener(e -> {
-
-	         Your_Choice1.updateSeat1(seat2.getText());
-
-	      });
-		add(seat1);
-		add(seat2);
-		add(title);
+		setLayout(new FlowLayout());
+//		getFrame().setVisible(true);
 	}
 }
 
+//극장, 날짜, 시간 라벨
+class Count {
+	public static String Count(String text) {
+		return text;
+	}
+}
 
 //극장, 날짜, 시간 라벨
 class Seat_Label extends JLabel {
-
- public void Set_Text(String text) {
-    setText(text);
-    Font font = this.getFont();
-    setFont(new Font(font.getFontName(), Font.BOLD, 15));
- }
+	public void Set_Text(String text) {
+		setText(text);
+		Font font = this.getFont();
+		setFont(new Font(font.getFontName(), Font.BOLD, 15));
+	}
 }
 
 //좌석표시
@@ -173,7 +227,7 @@ class Your_Choice1 extends JPanel {
 	   private static Seat_Label a1;
 
 	   public Your_Choice1() {
-	      setBounds(950, 180, 200, 300);
+	      setBounds(950, 450, 200, 300);
 	      setLayout(null);
 	      setBackground(Color.white);
 	      setBorder(new LineBorder(Color.BLACK, 2));
@@ -185,19 +239,18 @@ class Your_Choice1 extends JPanel {
 
 	      info.setBounds(25, 0, 200, 50);
 	      
-	      a1.setBounds(10, 80, 120, 100);
 	      
-
+	      a1.setBounds(10, 80, 120, 500);
+	      
+	      
 	      add(info);
 	      add(a1);
-	      
-	     
 	   }
 	 
 
 	   public static void updateSeat1(String seat) {
 	      a1.Set_Text(seat);
-	      Your_ticket.seat.Set_Text("좌석:         "+seat);
+	      Your_ticket.seat.Set_Text("좌석: "+seat);
 	   }
 	   
 	   public static String get_seat() {
@@ -225,13 +278,21 @@ public class Seat extends JFrame {
         
         // 결제창 넘어가기
         JButton payment = new JButton("결제하기");
-        payment.setBounds(950, 625, 200, 50);
+        payment.setBounds(600, 625, 200, 50);
         payment.addActionListener(new ActionListener() {
            public void actionPerformed(ActionEvent e) {
+        	  if(Adult_Panel.get_Person() == 0)
+    				JOptionPane.showMessageDialog(null, "인원이 0명입니다.", "경고 메시지", JOptionPane.WARNING_MESSAGE);
+        	  else if(Adult_Panel.get_Person() != PersonPanel.checked) {
+  				JOptionPane.showMessageDialog(null, "인원과 티켓 장수가 맞지 않습니다.", "경고 메시지", JOptionPane.WARNING_MESSAGE);
+        	  }else {
               Payment frame1 = new Payment();
-              frame.dispose(); // 현재 프레임 종료
+              frame.dispose();
+        	  }
            }
         });
+        
+        
         add(payment);
         add(previous);
         add(new Adult_Panel());
